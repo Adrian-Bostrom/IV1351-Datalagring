fa73862e70e4aedcc5135622a8b8b3803b82ef2e
visand-kth <visand@kth.se>
2024-11-21 18:57:39 +0000

    Database creation, no data yet tho

--- soundgood_music.sql
+++ soundgood_music.sql
@@ -25,13 +25,13 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.student (
-    student_id serial,
+    student_id serial PRIMARY KEY,
     sibling_id integer,
     contact_id integer --FK
 );
 
 CREATE TABLE public.contact_person (
-    contact_id serial,
+    contact_id serial PRIMARY KEY,
     relation varchar(50) NOT NULL,
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
@@ -40,18 +40,18 @@ CREATE TABLE public.contact_person (
 );
 
 CREATE TABLE public.instructor (
-    person_id serial,
-    ensamble_proficiency BOOL NOT NULL -- TODO is bool a thing?
+    person_id serial PRIMARY KEY,
+    ensamble_proficiency BOOLEAN NOT NULL -- TODO is bool a thing?
 );
 
 CREATE TABLE public.instructor_instruments (
-    instrument_instructable_id serial,
+    instrument_instructable_id serial PRIMARY KEY,
     person_id integer NOT NULL, --FK
     instrument varchar(30) NOT NULL
 );
 
 CREATE TABLE public.person (
-    id serial,
+    id serial PRIMARY KEY,
     person_number varchar(12) NOT NULL,
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
@@ -66,15 +66,15 @@ CREATE TABLE public.address (
 );
 
 CREATE TABLE public.rental_record (
-    rent_id serial,
-    rent_start date NOT NULL,
-    rent_end date,
+    rent_id serial PRIMARY KEY,
+    rent_start DATE NOT NULL,
+    rent_end DATE,
     instrument_id integer NOT NULL, --FK
     person_id integer NOT NULL --FK
-)
+);
 
 CREATE TABLE public.renting_instrument (
-    instrument_id serial,
+    instrument_id serial PRIMARY KEY,
     stock integer NOT NULL,
     type varchar(20) NOT NULL,
     brand varchar(30) NOT NULL,
@@ -82,15 +82,15 @@ CREATE TABLE public.renting_instrument (
 );
 
 CREATE TABLE public.lesson (
-    lesson_id serial,
-    time time(10) NOT NULL, -- TODO check time types
-    date date NOT NULL,
+    lesson_id serial PRIMARY KEY,
+    time TIME(6) NOT NULL, -- TODO check time types
+    date DATE NOT NULL,
     price_id integer NOT NULL, --FK
     person_id integer NOT NULL --FK
 );
 
 CREATE TABLE public.group_lesson (
-    lesson_id serial, --FK (inherited?)
+    lesson_id serial PRIMARY KEY, --FK (inherited?)
     min_enrollments integer NOT NULL,
     max_places integer NOT NULL,
     lesson_level integer NOT NULL,
@@ -98,21 +98,25 @@ CREATE TABLE public.group_lesson (
 );
 
 CREATE TABLE public.individual_lesson (
-    lesson_id serial, --FK (inherited?)
+    lesson_id serial PRIMARY KEY, --FK (inherited?)
     lesson_level integer NOT NULL,
     instrument varchar(30)
 );
 
 CREATE TABLE public.ensemble (
-    lesson_id serial, --FK (inherited?)
+    lesson_id serial PRIMARY KEY, --FK (inherited?)
     genre varchar(10) NOT NULL,
     min_num_students integer NOT NULL,
     max_num_students integer NOT NULL
 );
 
 CREATE TABLE public.pricing_scheme (
-    price_id serial,
+    price_id serial PRIMARY KEY,
     price float(10) NOT NULL,
-    change_date date NOT NULL
+    change_date DATE NOT NULL
 );
 
+--WITH person_id AS
+--(INSERT INTO public.person(person_number, first_name, last_name, phone, email)
+--VALUES ('192322059135', 'Goran', 'Persson', '0723682068', 'goran.persson@gmail.com')
+--RETURNING id),
