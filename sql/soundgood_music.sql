222b66113be7081396a98269ee924a342a906a90
Lucas Rimfrost <lucas.rimfrost@gmail.com>
2025-01-05 17:13:24 +0100

    Changed SERIAL to INT GENERATED ALWAYS AS IDENTITY in soundgood_music.sql

--- sql/soundgood_music.sql
+++ sql/soundgood_music.sql
@@ -1,5 +1,5 @@
 CREATE TABLE public.contact_person (
-    contact_id serial PRIMARY KEY,
+    contact_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     relation varchar(500) NOT NULL,
     first_name varchar(500) NOT NULL,
     last_name varchar(500) NOT NULL,
@@ -8,29 +8,29 @@ CREATE TABLE public.contact_person (
 );
 
 CREATE TABLE public.student (
-    student_id serial PRIMARY KEY,
+    student_id INT PRIMARY KEY,
     sibling_id INT,
     contact_id INT --references contact_person(contact_id) --FK
 );
 
 CREATE TABLE public.student_attending (
-    student_id serial,
-    lesson_id serial
+    student_id INT,
+    lesson_id INT
 );
 
 CREATE TABLE public.instructor (
-    instructor_id serial PRIMARY KEY,
+    instructor_id INT PRIMARY KEY,
     ensamble_proficiency BOOLEAN NOT NULL
 );
 
 CREATE TABLE public.instructor_instruments (
-    instrument_instructable_id serial PRIMARY KEY,
+    instrument_instructable_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     person_id INT, -- NOT NULL references person(id), --FK
     instrument varchar(500) NOT NULL
 );
 
 CREATE TABLE public.person (
-    person_id serial PRIMARY KEY,
+    person_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     person_number varchar(12) NOT NULL,
     first_name varchar(500) NOT NULL,
     last_name varchar(500) NOT NULL,
@@ -40,14 +40,14 @@ CREATE TABLE public.person (
 );
 
 CREATE TABLE public.address (
-    address_id serial PRIMARY KEY,
+    address_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     zip varchar(500) NOT NULL,
     city varchar(500) NOT NULL,
     street varchar(500) NOT NULL
 );
 
 CREATE TABLE public.renting_instrument (
-    instrument_id serial PRIMARY KEY,
+    instrument_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     stock INT NOT NULL,
     type varchar(500) NOT NULL,
     brand varchar(500) NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE public.renting_instrument (
 );
 
 CREATE TABLE public.rental_record (
-    rent_id serial PRIMARY KEY,
+    rent_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     rent_start DATE NOT NULL,
     rent_end DATE,
     instrument_id INT, --references renting_instrument(instrument_id) NOT NULL, --FK
@@ -63,7 +63,7 @@ CREATE TABLE public.rental_record (
 );
 
 CREATE TABLE public.lesson (
-    lesson_id serial PRIMARY KEY,
+    lesson_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     time TIME(6) NOT NULL,
     date DATE NOT NULL,
     price_id INT NOT NULL, --FK
@@ -71,7 +71,7 @@ CREATE TABLE public.lesson (
 );
 
 CREATE TABLE public.group_lesson (
-    lesson_id serial PRIMARY KEY, --FK (inherited?)
+    lesson_id INT PRIMARY KEY, --FK (inherited?)
     min_enrollments INT NOT NULL,
     max_places INT NOT NULL,
     lesson_level INT NOT NULL,
@@ -79,20 +79,20 @@ CREATE TABLE public.group_lesson (
 );
 
 CREATE TABLE public.individual_lesson (
-    lesson_id serial PRIMARY KEY, --FK (inherited?)
+    lesson_id INT PRIMARY KEY, --FK (inherited?)
     lesson_level INT NOT NULL,
     instrument varchar(500)
 );
 
 CREATE TABLE public.ensemble (
-    lesson_id serial PRIMARY KEY, --FK (inherited?)
+    lesson_id INT PRIMARY KEY, --FK (inherited?)
     genre varchar(500) NOT NULL,
     min_num_students INT NOT NULL,
     max_num_students INT NOT NULL
 );
 
 CREATE TABLE public.pricing_scheme (
-    price_id serial PRIMARY KEY,
+    price_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     price decimal(10,2) NOT NULL,
     change_date DATE NOT NULL,
     lesson_level INT NOT NULL,
@@ -128,7 +128,7 @@ REFERENCES public.person(person_id);
 ALTER TABLE public.instructor
 ADD CONSTRAINT instructor_id
 FOREIGN KEY (instructor_id)
-REFERENCES public.person(person_id);    
+REFERENCES public.person(person_id);
 
 ALTER TABLE public.lesson
 ADD CONSTRAINT person_id
