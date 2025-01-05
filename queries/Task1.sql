92bcdd8118a7c2a9c6976d1bca72908bb8c346db
Lucas Rimfrost <lucas.rimfrost@gmail.com>
2025-01-05 16:35:08 +0100

    Added DROP TABLE for Task1.sql

--- queries/Task1.sql
+++ queries/Task1.sql
@@ -1,61 +1,62 @@
+DROP TABLE IF EXISTS individual_count_temp, ensemble_count_temp, group_count_temp;
 
 -- Create a temp table for individual lesson divided by count per month
 CREATE TEMP TABLE individual_count_temp AS
-SELECT 
+SELECT
     DATE_TRUNC('month', date) AS month,
     COUNT(*) AS individual_count
-FROM 
+FROM
     public.lesson
-RIGHT JOIN 
+RIGHT JOIN
     public.individual_lesson
-ON 
+ON
     public.lesson.lesson_id = public.individual_lesson.lesson_id
-GROUP BY 
+GROUP BY
     month
-ORDER BY 
+ORDER BY
     month;
 
 -- Create a temp table for ensembles divided by count per month
 
 CREATE TEMP TABLE ensemble_count_temp AS
-SELECT 
+SELECT
     DATE_TRUNC('month', date) AS month,
     COUNT(*) AS ensemble_count
-FROM 
+FROM
     public.lesson
-RIGHT JOIN 
+RIGHT JOIN
     public.ensemble
-ON 
+ON
     public.lesson.lesson_id = public.ensemble.lesson_id
-GROUP BY 
+GROUP BY
     month
-ORDER BY 
+ORDER BY
     month;
 
 -- Create a temp table for group lesson divided by count per month
 
 CREATE TEMP TABLE group_count_temp AS
-SELECT 
+SELECT
     DATE_TRUNC('month', date) AS month,
     COUNT(*) AS group_count
-FROM 
+FROM
     public.lesson
-RIGHT JOIN 
+RIGHT JOIN
     public.group_lesson
-ON 
+ON
     public.lesson.lesson_id = public.group_lesson.lesson_id
-GROUP BY 
+GROUP BY
     month
-ORDER BY 
+ORDER BY
     month;
 
 
 -- Join temp tables:
 
-SELECT 
+SELECT
     to_char(COALESCE(
-        individual_count_temp.month, 
-        ensemble_count_temp.month, 
+        individual_count_temp.month,
+        ensemble_count_temp.month,
         group_count_temp.month
     ), 'Month') AS month,
     (individual_count_temp.individual_count +ensemble_count_temp.ensemble_count + group_count_temp.group_count)
@@ -63,25 +64,24 @@ SELECT
     COALESCE(individual_count_temp.individual_count,0) AS individual_count,
     COALESCE(ensemble_count_temp.ensemble_count,0) AS ensemble_count,
     COALESCE(group_count_temp.group_count,0) AS group_count
-FROM 
+FROM
     individual_count_temp
-FULL OUTER JOIN 
-    ensemble_count_temp 
-ON 
+FULL OUTER JOIN
+    ensemble_count_temp
+ON
     individual_count_temp.month = ensemble_count_temp.month
-FULL OUTER JOIN 
-    group_count_temp 
-ON 
+FULL OUTER JOIN
+    group_count_temp
+ON
     COALESCE(
-        individual_count_temp.month, 
+        individual_count_temp.month,
         ensemble_count_temp.month
-    ) = group_count_temp.month 
-WHERE 
+    ) = group_count_temp.month
+WHERE
     EXTRACT(
         YEAR FROM COALESCE(
-            individual_count_temp.month, 
-            ensemble_count_temp.month, 
+            individual_count_temp.month,
+            ensemble_count_temp.month,
             group_count_temp.month
         )
     ) = 2024;
-    
