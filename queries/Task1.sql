5c73c5e64a09b517f2b8154bdf8b48d17c92d535
Adrian-Bostrom <bostromadrian@gmail.com>
2025-01-05 16:52:41 +0100

    Removed redundant lines from print in task 1

--- queries/Task1.sql
+++ queries/Task1.sql
@@ -1,87 +1,59 @@
-DROP TABLE IF EXISTS individual_count_temp, ensemble_count_temp, group_count_temp;
-
--- Create a temp table for individual lesson divided by count per month
-CREATE TEMP TABLE individual_count_temp AS
+WITH individual_count AS (
+    SELECT
+        DATE_TRUNC('month', date) AS month,
+        COUNT(*) AS individual_count
+    FROM
+        public.lesson
+    RIGHT JOIN
+        public.individual_lesson
+    ON
+        public.lesson.lesson_id = public.individual_lesson.lesson_id
+    GROUP BY
+        month
+    ORDER BY
+        month
+),
+ensemble_count AS (
+    SELECT
+        DATE_TRUNC('month', date) AS month,
+        COUNT(*) AS ensemble_count
+    FROM
+        public.lesson
+    RIGHT JOIN
+        public.ensemble
+    ON
+        public.lesson.lesson_id = public.ensemble.lesson_id
+    GROUP BY
+        month
+    ORDER BY
+        month
+),
+group_count AS (
+    SELECT
+        DATE_TRUNC('month', date) AS month,
+        COUNT(*) AS group_count
+    FROM
+        public.lesson
+    RIGHT JOIN
+        public.group_lesson
+    ON
+        public.lesson.lesson_id = public.group_lesson.lesson_id
+    GROUP BY
+        month
+    ORDER BY
+        month
+)
 SELECT
-    DATE_TRUNC('month', date) AS month,
-    COUNT(*) AS individual_count
+    to_char(COALESCE(i.month, e.month, g.month), 'Month') AS month,
+    (COALESCE(i.individual_count, 0) + COALESCE(e.ensemble_count, 0) + COALESCE(g.group_count, 0)) AS Total,
+    COALESCE(i.individual_count, 0) AS individual_count,
+    COALESCE(e.ensemble_count, 0) AS ensemble_count,
+    COALESCE(g.group_count, 0) AS group_count
 FROM
-    public.lesson
-RIGHT JOIN
-    public.individual_lesson
-ON
-    public.lesson.lesson_id = public.individual_lesson.lesson_id
-GROUP BY
-    month
-ORDER BY
-    month;
-
--- Create a temp table for ensembles divided by count per month
-
-CREATE TEMP TABLE ensemble_count_temp AS
-SELECT
-    DATE_TRUNC('month', date) AS month,
-    COUNT(*) AS ensemble_count
-FROM
-    public.lesson
-RIGHT JOIN
-    public.ensemble
-ON
-    public.lesson.lesson_id = public.ensemble.lesson_id
-GROUP BY
-    month
-ORDER BY
-    month;
-
--- Create a temp table for group lesson divided by count per month
-
-CREATE TEMP TABLE group_count_temp AS
-SELECT
-    DATE_TRUNC('month', date) AS month,
-    COUNT(*) AS group_count
-FROM
-    public.lesson
-RIGHT JOIN
-    public.group_lesson
-ON
-    public.lesson.lesson_id = public.group_lesson.lesson_id
-GROUP BY
-    month
-ORDER BY
-    month;
-
-
--- Join temp tables:
-
-SELECT
-    to_char(COALESCE(
-        individual_count_temp.month,
-        ensemble_count_temp.month,
-        group_count_temp.month
-    ), 'Month') AS month,
-    (individual_count_temp.individual_count +ensemble_count_temp.ensemble_count + group_count_temp.group_count)
-    AS Total,
-    COALESCE(individual_count_temp.individual_count,0) AS individual_count,
-    COALESCE(ensemble_count_temp.ensemble_count,0) AS ensemble_count,
-    COALESCE(group_count_temp.group_count,0) AS group_count
-FROM
-    individual_count_temp
+    individual_count i
 FULL OUTER JOIN
-    ensemble_count_temp
-ON
-    individual_count_temp.month = ensemble_count_temp.month
+    ensemble_count e ON i.month = e.month
 FULL OUTER JOIN
-    group_count_temp
-ON
-    COALESCE(
-        individual_count_temp.month,
-        ensemble_count_temp.month
-    ) = group_count_temp.month
+    group_count g ON COALESCE(i.month, e.month) = g.month
 WHERE
-    EXTRACT(
-        YEAR FROM COALESCE(
-            individual_count_temp.month,
-            ensemble_count_temp.month,
-            group_count_temp.month
-        )
-    ) = 2024;
+    EXTRACT(YEAR FROM COALESCE(i.month, e.month, g.month)) = EXTRACT(YEAR FROM CURRENT_DATE);
